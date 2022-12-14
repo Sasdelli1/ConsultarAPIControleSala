d5676272667919a44a7df97883c83169586959bd
gabriel.fsasdelli <gabriel.fsasdelli@OSA0557593W10-1>
2022-12-13 21:01:05 -0300

    Adicionar arquivos de projeto.

---
+++ Consulta/ConsultaAlunos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace ConsultarAPIControleSala.Consulta
+{
+    class ConsultaAlunos
+    {
+        public string Consulta(string nome)
+        {
+            string oURL = "https://localhost:5001/api/Aluno?p_nome_aluno=" + nome;
+            HttpClient _httpClient = new HttpClient();
+            HttpResponseMessage result =_httpClient.GetAsync(oURL).Result;
+            String JsonRetorno = result.Content.ReadAsStringAsync().Result;
+            return JsonRetorno;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsultarAPIControleSala.Consulta;
+using System;
+
+namespace ConsultarAPIControleSala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsultaAlunos Consulta1 = new ConsultaAlunos();
+
+            Console.WriteLine("Consulta de Alunos");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Digite o nome do Aluno que deseja buscar:");
+            Console.WriteLine("-----------------------------------------");
+            string NomeAluno = Console.ReadLine();
+
+            string Resultado = Consulta1.Consulta(NomeAluno);
+
+            Console.WriteLine(Resultado);
+
+        }
+    }
+}
